85369b88d318def954061368c638ab41265e311f
Ethan Lin <ethan.lin@uwex.edu>
2017-06-16 09:16:25 -0500

    updated HTML structures and layouts

--- scripts/uls-badge-maker-dev.js
+++ scripts/uls-badge-maker-dev.js
@@ -1,68 +1,168 @@
+// ON DOM READY
 $( function() {
     
-    // HTML view variables
-    var numColumn = 3;
     
-    $.get("fonts/badge_bg.svg", function(data) {
+    var layout = {
         
-        var glyphs = $( data ).find( 'g' );
+        wrapper: $( '#uls-badge-maker' ),
+        header: $( '#uls-badge-maker header' ),
+        canvas: $( '#uls-badge-maker .badge-body .canvas-wrapper' ),
+        tabContainer: $( '#uls-badge-maker .badge-body .badge-container' ),
+        tabControl: $( '#uls-badge-maker .badge-body .badge-container .tab-control' ),
+        tabItem: $( '#uls-badge-maker .badge-body .badge-container .tab-control .tab-item' ),
+        tabContent: $( '#uls-badge-maker .badge-body .badge-container .tab-content' ),
+        ctaWrapper: $( '#uls-badge-maker .badge-body .cta' ),
+        footer: $( '#uls-badge-maker footer' )
         
-        // svg sprite variables
-        var x = 0;
-        var y = 0;
-        var width = 198;
-        var height = 230;
-        var gutter = 10;
+    };
+    
+    calcLayout();
+    $( window ).on( 'resize', calcLayout );
+    
+    // number of columns in the badge background SVG file
+    var numBGColumns = 2;
+    
+    // number of columns in the badge SVG file
+    var numBadgeColumns = 4;
+    
+    // spacing between each column
+    var gutter = 10;
+    
+    // get badge background icons at first
+    getBadgeFromSVG( 'fonts/badge_bg.svg', numBGColumns, gutter);
+    
+    // tab selection event
+    layout.tabItem.on( 'click', function( e ) {
         
-        $.each( glyphs, function( col ) {
+        var currentTarget = $( e.currentTarget );
+        
+        if ( !currentTarget.hasClass( 'active' ) ) {
             
-            var path = $( glyphs[col] ).find( 'path' );
-            var svg = document.createElementNS( 'http://www.w3.org/2000/svg', 'svg');
-            var badgeWrapperDiv = document.createElement( 'div' );
-            var badgeDiv = document.createElement( 'div' );
+            // set next tab
+            var next = currentTarget.data( 'target' );
             
-            badgeWrapperDiv.classList.add( 'badge-wrapper' );
-            badgeDiv.classList.add('badge');
+            // reset tabs
+            layout.tabItem.removeClass( 'active' );
+            currentTarget.addClass( 'active' );
+            layout.tabContent.empty();
             
-            svg.setAttribute( 'width', "100%" );
-            svg.setAttribute( 'height', "100%" );
-            
-            if ( col > 0 ) {
-                
-                if ( ( col % numColumn ) === 0 ) {
-                    
-                    x = 0;
-                    y = y + height + gutter;
-                    
-                } else {
-                    
-                    x = x + width + gutter;
-                    
-                }
-                
-            } else {
-                
-                x = 0;
-                
+            // get content
+            switch (next) {
+                case 'badges':
+                    getBadgeFromSVG( 'fonts/badges.svg', numBadgeColumns, gutter );
+                    getBadgeFromSVG( 'fonts/badges.svg', numBadgeColumns, gutter );
+                break;
+                case 'badge_bgs':
+                    getBadgeFromSVG( 'fonts/badge_bg.svg', numBGColumns, gutter );
+                break;
+                case 'colors':
+                break;
             }
-
-            $.each( path, function(i) {
-                
-                var d = $( path[i] ).attr( 'd' );
-                
-                svg.innerHTML += '<path d="' + d + '" />';
-                svg.setAttribute( 'viewBox', x + ' ' + y + ' ' + width + ' ' + height );
-                svg.setAttribute('preserveAspectRatio', 'xMidYMin');   
-                
-            } );
             
-            badgeDiv.innerHTML = new XMLSerializer().serializeToString(svg);
-            badgeWrapperDiv.innerHTML = new XMLSerializer().serializeToString( badgeDiv );
-            
-            $( '.badge_bg_container' ).append( badgeWrapperDiv );
-            
-        } );
+        }
         
     } );
     
-} );
+    // read and badges from the SVG file
+    function getBadgeFromSVG( file, columns, spacing ) {
+        
+        $.get( file, function( data ) {
+        
+            var svgViewBox = $( data ).find( 'svg' ).attr( 'viewBox' ).split( ' ' );
+            var glyphs = $( data ).find( 'g[id]' );
+            var numRows = 0;
+            
+            // get number of rows
+            for ( var j = 0; j < glyphs.length; j++ ) {
+                
+                if ( ( j % numBadgeColumns ) === 0 ) {
+                    numRows++;
+                }
+                
+            }
+            
+            // calculate width and height per sprite
+            var w = ( Number( svgViewBox[2] ) - ( spacing * ( columns - 1 ) ) ) / columns;
+            var h = Number( svgViewBox[3] );
+            
+            if ( numRows >= 2 ) {
+                h = ( Number( svgViewBox[3] ) - ( spacing * ( numRows - 1 ) ) ) / numRows;
+            }
+            
+            // set viewbox for each sprite
+            var viewBox = {
+                
+                x: Number( svgViewBox[0] ),
+                y: Number ( svgViewBox[1] ),
+                width: w,
+                height: h
+                
+            };
+            
+            // get each sprite
+            $.each( glyphs, function( i ) {
+                
+                var svg = document.createElementNS( 'http://www.w3.org/2000/svg', 'svg');
+                var badgeWrapperDiv = document.createElement( 'div' );
+                var badgeDiv = document.createElement( 'div' );
+                
+                var context = $( glyphs[i] ).html();
+                
+                badgeWrapperDiv.classList.add( 'badges' );
+                badgeDiv.classList.add( 'badge' );
+                
+                svg.setAttribute( 'width', "100%" );
+                svg.setAttribute( 'height', "100%" );
+                
+                if ( i > 0 ) {
+                    
+                    if ( ( i % columns ) === 0 ) {
+                        
+                        viewBox.x = 0;
+                        viewBox.y = viewBox.y + viewBox.height + spacing;
+                        
+                    } else {
+                        
+                        viewBox.x = viewBox.x + viewBox.width + spacing;
+                        
+                    }
+                    
+                } else {
+                    
+                    viewBox.x = 0;
+                    
+                }
+                
+                svg.innerHTML = context;
+                svg.setAttribute( 'viewBox', viewBox.x + ' ' + viewBox.y + ' ' + viewBox.width + ' ' + viewBox.height );
+                svg.setAttribute('preserveAspectRatio', 'xMidYMid meet');
+                
+                badgeDiv.innerHTML = new XMLSerializer().serializeToString(svg);
+                badgeWrapperDiv.innerHTML = new XMLSerializer().serializeToString( badgeDiv );
+                
+                layout.tabContent.append( badgeWrapperDiv );
+                
+            } );
+            
+        } );
+        
+    }
+    
+    function calcLayout() {
+        
+        if ( $( document ).width() < 1025 ) {
+            
+            var tContainerInverseHeight = layout.canvas.outerHeight();
+        
+            layout.tabContainer.css(
+                'height',
+                'calc(100% - ' + tContainerInverseHeight + 'px)'
+            );
+            
+        } else {
+            layout.tabContainer.css( 'height', '' );
+        }
+        
+    }
+    
+} ); // DOM READY END
